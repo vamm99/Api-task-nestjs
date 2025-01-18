0adc662eceb9d25b90caf54df332dfc120aae981
Víctor Alfonso Mejía Morales <130195315+vamm99@users.noreply.github.com>
2025-01-18 09:42:08 -0500

    feat: Extender Service para logear las acciones en el servicio

--- src/module/task/task.service.ts
+++ src/module/task/task.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Service } from 'src/service';
 
 @Injectable()
-export class TaskService {
+export class TaskService extends Service {
+  constructor() {
+    super(TaskService.name);
+  }
   create(createTaskDto: CreateTaskDto) {
+    this.logger.log('Creating a new task');
     return 'This action adds a new task';
   }
 
   findAll() {
+    this.logger.log('Getting all tasks from the database');
     return `This action returns all task`;
   }
 
   findOne(id: number) {
+    this.logger.log(`Getting task with id ${id}`);
     return `This action returns a #${id} task`;
   }
 
   update(id: number, updateTaskDto: UpdateTaskDto) {
+    this.logger.log(`Updating task with id ${id}`);
     return `This action updates a #${id} task`;
   }
 
   remove(id: number) {
+    this.logger.log(`Removing task with id ${id}`);
     return `This action removes a #${id} task`;
   }
 }
